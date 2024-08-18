4482406dd8c519c28933455f899e4e4bb5dd00ee
Sahaj Jain <sahaj.jain@myhq.in>
2024-08-19 05:20:39 +0530

    feat: Update content with projects

--- src/components/Age.tsx
+++
@@ -1,6 +0,0 @@
-import getYearsSinceDate from "../utils/get-years-since-date";
-
-export default function Age() {
-    const BIRTH_DATE_ISO = "2001-07-03T00:00:00Z";
-    return <span>{getYearsSinceDate(BIRTH_DATE_ISO)}y old</span>;
-}
