8bd754a65659dc027358243b6a9e66696199a882
Sahaj Jain <sahaj.jain@myhq.in>
2024-08-20 13:59:01 +0530

    feat: Add rss support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
@@ -7,5 +7,9 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), icon(), mdx()]
+    integrations: [tailwind(), react(), icon(), mdx()],
+    image: {
+        service: passthroughImageService(),
+    },
 });
+
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,27 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+    const blog = await getCollection("blog", ({ data }) => {
+        return data.draft !== true;
+    });
+
+    return rss({
+        site: context.url.origin,
+        title: "iamsahaj.xyz",
+        description: "iamsahaj.xyz",
+        items: blog.map((post) => ({
+            title: post.data.title,
+            description: post.data.description,
+            pubDate: post.data.publishDate,
+            link: `/blog/${post.slug}/`,
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+            }),
+        })),
+        customData: `<language>en-us</language>`,
+    });
+}
